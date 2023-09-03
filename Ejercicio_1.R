22d6fc4325d1a1ae0dde76fb0474e7bbbc37bc9a
Cande <cande.russof2@gmail.com>
2023-09-03 17:47:53 -0300

    Ej 1 y 2

--- Ejercicio_1.R
+++ Ejercicio_1.R
@@ -1,6 +1,7 @@
 #--------- Ejercicio 1 ---------
 #Seteo mi directorio
-setwd("/home/clinux01/Escritorio/Cande Labo Jueves/Practica_2/")
+#setwd("/home/clinux01/Escritorio/Cande Labo Jueves/Practica_2/")
+setwd("/Users/Usuario/Desktop/Cande/Laboratorio de Procesamiento de Información meteorológica/Practica_2/")
 rm(list=ls()) #Limpio mi environment
 
 #--------- Inciso a ---------
@@ -14,3 +15,20 @@ a<-(3<4) #Da T #Al a ser una variable logica, los resultados me aparecen en el e
 a<-(b<=c) #Da T
 a<-(b==c) #Da F
 a<-(b!=c) #Da T
+
+#--------- Inciso b ---------
+rm(list=ls())
+a=T
+b=F
+
+#Pruebo comandos
+islogical(a) #No funciona, lo corrijo
+is.logical(a) #Da T q tiene sentido 
+
+(a&b) #Da F
+
+(a|b) #Da T
+
+(!b) #Da T porque es la negación de b y b es F
+
+is.logical(a|b) #Da T
