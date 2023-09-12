a4a4ec0f32f5e767c1af22c802665d0404d28dc5
Cande <cande.russof2@gmail.com>
2023-09-12 16:35:34 -0300

    Ej 5

--- Ejercicio_5.R
+++ Ejercicio_5.R
@@ -1,22 +1,33 @@
-#---------------------- Ejercicio 5. Práctica 2 ----------------------
+#---------------------- Ejercicio 5. Practica 2 ----------------------
 #Seteo el directorio y limpio el environment
-setwd("/home/clinux01/Escritorio/Labo/Practica_2/")
+#setwd("/home/clinux01/Escritorio/Labo/Practica_2/")
+setwd("/home/clinux01/Escritorio/Cande/Practica_2/")
 rm(list=ls())
 
 #---------------------- Inciso a ----------------------
 #Ingreso de mis variables
-a<-as.numeric(readline("Ingrese el número A"))
-b<-as.numeric(readline("Ingrese el número B"))
+a<-as.numeric(readline("Ingrese el numero A"))
+b<-as.numeric(readline("Ingrese el numero B"))
 a1<-as.integer(a)
 b1<-as.integer(b)
-c=5 #Acá debería poner la formula para chequear que sean múltiplos
+c=5 #Aca deberia poner la formula para chequear que sean multiplos
 
-if (a!=a1 | b!=b1){
-  print("Ingrese un numero entero")
-} else {
-  if(c==5){
-    print("Sus números son múltiplos")
-  } else {
-  print("Los números no son múltiplos")
-  }
+if (a!=a1 | b!=b1){ 
+  while(a!=a1){
+    readline("Ingrese un numero A ")}
+    if(a==a1){
+  while(b!=b1){
+    if(b==b1) {
+      break
+    }
+    b<-as.numeric(readline("Ingrese un numero B"))}
+    b1<-as.integer(b)
+    } 
 }
+  if(a==a1& b==b1){
+    if(c==5){
+    print("Sus numeros son multiplos")
+  } 
+    } else {
+  print("Los numeros no son multiplos")
+  }
