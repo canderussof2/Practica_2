17b4eb3a1a848e55269252974db89cdf4deadb56
Cande <cande.russof2@gmail.com>
2023-10-13 15:36:56 -0300

    Preguntar sin vectores

--- Ej_15.R
+++ Ej_15.R
@@ -5,19 +5,6 @@ setwd("/Users/Usuario/Desktop/Cande/Laboratorio de Procesamiento de Información
 
 #Prog pida ingreso nombre y apellido de un estudiante y la cantidad de materias
 #aprobo. Luego pedir el ingreso nombre de materia y nota que obtuvo. 
-
-apellido<-readline("Ingrese su apellido ")
-nombre<-readline("Ingrese su nombre ")
-
-cant_materias<-as.integer(readline("Ingrese la cantidad de materias aprobadas "))
-while(is.na(cant_materias)){
-  if(!is.na(cant_materias)){
-    break
-  } else {
-cant_materias<-as.integer(readline("Ingrese nuevamente la cantidad de materias aprobadas "))
- } 
-}
-
 #Finalmente, imprimir el siguiente mensaje (Sin las comillas):
 #  - “El estudiante NOMBRE y APELLIDO aprobo X materias y su promedio es mayor al promedio
 #historico”
@@ -31,3 +18,94 @@ cant_materias<-as.integer(readline("Ingrese nuevamente la cantidad de materias a
 #El programa debe verificar en cada instancia que la cantidad de materias y la nota obtenida en
 #cada caso sean numeros y en caso contrario debe pedir su reingreso.
 #* aclaracion: Para resolver este ejercicio no deben usarse nociones de vectores
+
+apellido<-readline("Ingrese su apellido ")
+nombre<-readline("Ingrese su nombre ")
+
+cant_materias<-as.integer(readline("Ingrese la cantidad de materias aprobadas "))
+while(is.na(cant_materias)){
+  if(!is.na(cant_materias)){
+    break
+  } else {
+cant_materias<-as.integer(readline("Ingrese nuevamente la cantidad de materias aprobadas "))
+ } 
+}
+
+if (cant_materias==0){
+  cat("El estudiante", nombre,apellido,"no aprobó materias","\n")
+} else {
+secuencia<-seq(1,cant_materias,by=1)
+
+notas<-c()
+for(valor in secuencia){
+  nota_materias<-as.integer(readline("Ingrese la nota de cada materia "))
+  while(is.na(nota_materias)){
+    if(!is.na(nota_materias)){
+      break
+    } else {
+      nota_materias<-as.integer(readline("Ingrese nuevamente la nota de cada materia "))
+    } 
+  }
+  notas<-c(notas,nota_materias)
+}
+promedio<-mean(notas)
+
+if(promedio>8.41){
+  cat("El estudiante", nombre,apellido,"aprobó",cant_materias,"materias y su promedio es mayor al promedio
+      historico","\n")
+} else if (promedio==8.41){
+  cat("El estudiante", nombre,apellido,"aprobó",cant_materias,"materias y su promedio es igual al promedio
+      historico","\n")
+} else if (promedio<8.41){
+  cat("El estudiante", nombre,apellido,"aprobó",cant_materias,"materias y su promedio es menor al promedio
+      historico","\n")
+ }
+}  
+#-------------------------------------------------------------------------------
+rm(list=ls())
+setwd("/Users/Usuario/Desktop/Cande/Laboratorio de Procesamiento de Información meteorológica/Practica_2/")
+
+#SIN VECTORES
+apellido<-readline("Ingrese su apellido ")
+nombre<-readline("Ingrese su nombre ")
+
+cant_materias<-as.integer(readline("Ingrese la cantidad de materias aprobadas "))
+while(is.na(cant_materias)){
+  if(!is.na(cant_materias)){
+    break
+  } else {
+    cant_materias<-as.integer(readline("Ingrese nuevamente la cantidad de materias aprobadas "))
+  } 
+}
+
+if (cant_materias==0){
+  cat("El estudiante", nombre,apellido,"no aprobó materias","\n")
+} else {
+  secuencia<-seq(1,cant_materias,by=1)
+  notas<-0
+  for(nota in secuencia){
+    notas[nota]<-as.integer(readline("Ingrese la nota de cada materia "))
+    while(is.na(cant_materias)){
+      if(!is.na(cant_materias)){
+        break
+      } else {
+       notas[nota]<-as.integer(readline("Ingrese nuevamente la nota de cada materia "))
+      } 
+    }
+  }
+  promedio<-notas/cant_materias
+  
+  if(promedio>8.41){
+    cat("El estudiante", nombre,apellido,"aprobó",cant_materias,"materias y su promedio es mayor al promedio
+      historico","\n")
+  } else if (promedio==8.41){
+    cat("El estudiante", nombre,apellido,"aprobó",cant_materias,"materias y su promedio es igual al promedio
+      historico","\n")
+  } else if (promedio<8.41){
+    cat("El estudiante", nombre,apellido,"aprobó",cant_materias,"materias y su promedio es menor al promedio
+      historico","\n")
+  }
+}  
+
+  
+  
